2c90a45db6547833ba5435ae964644fe4a09bbbf
fatimzahara <zahara.kone@gmail.com>
2015-02-18 19:02:19 +0100

    Code final projet HT2Evry
    Code final permettant de consulter les horaires en temps réel

--- js/main.js
+++ js/main.js
@@ -32,18 +32,121 @@ donnees.onreadystatechange=function(){
 donnees.open('GET','http://api.transilien.com/gare/87681387/depart/',true,'tnhtn218','2cAmwT93');
 donnees.send(null);
 */
+var listeGare = {
+87682138:'BOUSSY SAINT-ANTOINE',
+87681478:'BOUTIGNY',
+87681452:'LA FERTE ALAIS',
+87681627:'LE PLESSIS CHENET',
+87681155:'MAISONS ALFORT ALFORTVILLE',
+87682005:'MELUN',
+87681411:'MENNECY',
+87682104:'MONTGERON CROSNE',
+87276279:'ORRY LA VILLE COYE LA FORET',
+87682500:'VOSVES',
+87682161:'CESSON',
+87276113:'CHANTILLY GOUVIEUX',
+87681007:'CORBEIL ESSONNES',
+87681361:'EVRY - Val de Seine',
+87681387:'EVRY COURCOURONNES - Centre',
+87276196:'GARGES SARCELLES',
+87276253:'LOUVRES',
+87271163:'PIERREFITTE STAINS',
+87681858:'VILLENEUVE PRAIRIE',
+87681809:'VILLENEUVE TRIAGE',
+87276220:'VILLIERS LE BEL',
+87682120:'BRUNOY',
+87681510:'BUNO GIRONVILLE',
+87684407:'BOIGNEVILLE',
+87271023:'GARE DU NORD RER',
+87276246:'GOUSSAINVILLE',
+87681379:'GRIGNY CENTRE',
+87276287:'LA BORNE BLANCHE',
+87681635:'LE COUDRAY MONTCEAUX',
+87686030:'PARIS - GARE DE LYON',
+87682526:'PONTHIERRY PRINGY',
+87271015:'SAINT-DENIS',
+87681304:'VIGNEUX SUR SEINE',
+87681619:'VILLABE',
+87682518:'BOISSISE LE ROI',
+87682146:'COMBS LA VILLE QUINCY',
+87276006:'CREIL',
+87681601:'ESSONNES ROBINSON',
+87545244:'JUVISY',
+87682179:'LE MEE',
+87681247:'LE VERT DE MAISONS',
+87682153:'LIEUSAINT MOISSY',
+87681486:'MAISSE',
+87681403:'MOULIN GALANT',
+87681346:'ORANGIS BOIS DE LEPINE',
+87271031:'PARIS NORD (GARE DU NORD)',
+87682542:'SAINT-FARGEAU',
+87164780:'STADE DE FRANCE SAINT-DENIS',
+87276261:'SURVILLIERS FOSSES',
+87681312:'VIRY CHATILLON',
+87682112:'YERRES',
+87681338:'RIS ORANGIS',
+87682187:'SAVIGNY LE TEMPLE NANDY',
+87681825:'VILLENEUVE SAINT-GEORGES',
+87681437:'BALLANCOURT',
+87758607:'CHATELET LES HALLES',
+87681353:'GRAND BOURG',
+87681395:'LE BRAS DE FER - Evry Génopole',
+87276238:'LES NOUES',
+87684415:'MALESHERBES'
+};
+
+function getProchainsTrains(elm){
+  var gareId = $(elm).val();
+    $("#liste-horaires tbody").empty();
+    $.get("http://localhost/ProjetHT2Evry-master/php/testphp.php",{methode: "departs", gare: gareId}, function(data){
+      console.log(data);
+      horaires = data;
+         $(data).find('train').each(function(index,value){
+          console.log(value);
+            
+            $train = $(value);  
+            var line ="<tr>";
+          /*  line += "<td><span class='idx'>" + $train.find('num').html() + "</span></td>";*/
+            line += "<td><span class='date'>" + $train.find('date').html().split(' ')[1] + "</span></td>";
+            line += "<td><span class='miss'>" + $train.find('miss').html() + "</span></td>";
+            line += "<td><span class='term'>" + listeGare[$train.find('term').html()] + "</span></td>";
+            line += "</tr>";
+            $("#liste-horaires tbody").append($(line));
+         }
+          );
+
+    });
+}
 
 
-$.ajax
-({
-  type: "GET",
-  url: "http://api.transilien.com/gare/87681387/depart/",
-  async: true,
-  crossDomain: true,
-  username: "tnhtn218",
-  password: "2cAmwT93",
-  success: function (){
+function getItineraire(){
+  $("#message").html("");
+  var gareDep = $("#gareDep").val();
+  var gareArr = $("#gareArr").val();
+  if (gareDep == undefined || gareArr == undefined || gareDep == gareArr){
+    $("#message").html("Vérifiez vos parametres de recherche");
+    return; 
+  }
+    $("#liste-horaires tbody").empty();
+    $.get("http://localhost/ProjetHT2Evry-master/php/testphp.php",{methode: "itineraire", gareDep: gareDep, gareArr: gareArr, }, function(data){
+      if ( $(data).find('train').length ==0){
+        $("#message").html("Pas de train entre ces deux destinations");
+      }
+      else{
+         $(data).find('train').each(function(index,value){
+          console.log(value);
+            
+            $train = $(value);  
+            var line ="<tr>";
+           /* line += "<td><span class='idx'>" + $train.find('num').html() + "</span></td>";*/
+            line += "<td><span class='date'>" + $train.find('date').html().split(' ')[1] + "</span></td>";
+            line += "<td><span class='miss'>" + $train.find('miss').html() + "</span></td>";
+            line += "<td><span class='term'>" + listeGare[$train.find('term').html()] + "</span></td>";
+            line += "</tr>";
+            $("#liste-horaires tbody").append($(line));
+         }
+          );
+         }
 
-    alert('Thanks for your comment!');  
-  }  
-});
+    });
+}
--- php/testphp.php
+++ php/testphp.php
@@ -1,8 +1,20 @@
 <?PHP
-
+/*Identifiant pour acceder aux ressources sur API Transilien*/
 $username = "tnhtn218";
 $password = "2cAmwT93";
-$remote_url = 'http://api.transilien.com/gare/87681387/depart/';
+
+
+$methode = $_GET['methode'];
+$remote_url = "";
+switch($methode){
+    case("departs"):
+        default:
+        $remote_url = 'http://api.transilien.com/gare/'. $_GET['gare'].'/depart/';
+        break;
+    case('itineraire'):
+        $remote_url = 'http://api.transilien.com/gare/'. $_GET['gareDep'].'/depart/'. $_GET['gareArr'];
+        break;
+}
 
 // Create a stream
 $opts = array(
@@ -12,14 +24,24 @@ $opts = array(
   )
 );
 
+
 $context = stream_context_create($opts);
 
 // Open the file using the HTTP headers set above
 $file = file_get_contents($remote_url, false, $context);
 
+
+/*
 $newFile = new DOMDocument();
 $newFile -> loadXML($file);
+
 $newFile -> save('C:/Users/fatim/Documents/GitHub/ProjetHT2Evry/fichierXml.xml');
 print($file);
 
+*/
+
+header("Access-Control-Allow-Origin: *");
+//header ("Content-Type:text/xml");
+print($file);
+die;
 ?>
